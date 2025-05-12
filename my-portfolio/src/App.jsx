3e59a86546afd4c3081eca7d3f6abb75b450bb9c
Mila-Amen <million.degefu@dci-student.org>
2025-05-12 11:07:38 +0200

    First commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="font-sans text-gray-800">
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className="py-20 px-6 bg-gray-50">
+      <h2 className="text-3xl font-bold mb-6">About Me</h2>
+      <p>
+        I’m a junior web developer with a strong background in graphic design.
+        I love building responsive, user-friendly websites and blending creativity with clean code.
+      </p>
+    </section>
+  )
+}
+
+export default About
